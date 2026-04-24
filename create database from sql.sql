dd336dc74938b3cf343268d0f4cb9264a59f4660
pratham shah <92474926+pratham636@users.noreply.github.com>
2026-04-24 22:40:32 +0200

    Update create database from sql.sql

--- create database from sql.sql
+++ create database from sql.sql
@@ -4,7 +4,7 @@ USE expenses_tracker;
 -- id INT PRIMARY KEY AUTO_INCREMENT,
 -- name VARCHAR(100) NOT NULL ,
 -- price DECIMAL(10,2) NOT NULL,
--- catagory VARCHAR(50) NOT NULL,
+-- category VARCHAR(50) NOT NULL,
 -- date DATE NOT NULL);
 -- DELETE FROM expenses WHERE id=3;
 -- ALTER TABLE expenses auto_increment=1;
